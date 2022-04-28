12cbb8b760bf662bcbe29776e83c94ff78feb38a
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-28 08:49:19 +0700

    searcg

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -7,6 +7,10 @@ from django.core.paginator import Paginator
 def movie_list(request):
     movie_object = Movies.objects.all()
 
+    movie_name = request.GET.get('movie_name')
+    if movie_name != '' and movie_name is not None : 
+        movie_object = movie_object.filter(name__icontains = movie_name)
+
     paginator = Paginator(movie_object, 4)
     page = request.GET.get('page')
     movie_object = paginator.get_page(page)
