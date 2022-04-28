88fda491c66b09ff687de1b3613630b54acc0afe
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-28 08:34:49 +0700

    paginator

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Movies
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def movie_list(request):
     movie_object = Movies.objects.all()
+
+    paginator = Paginator(movie_object, 4)
+    page = request.GET.get('page')
+    movie_object = paginator.get_page(page)
+
+    return render(request, 'newapp/movies_list.html', {'movie_object':movie_object})
